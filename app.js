5e4378d3173000cad8e5b0c365675020efe8e7cd
7tabish <m.tabishmanzoor1@gmail.com>
2020-02-09 15:06:50 +0500

    nodejs docker app

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('<h3>Hello from node-js app</h3>');
+});
+
+app.listen(PORT,()=>console.log(`App is runnning on localhost:${PORT}`));
