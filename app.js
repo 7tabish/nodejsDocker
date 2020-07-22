0526b26cb6631920fad4c0760ca988f6473a623a
Tabish Manzoor <43508735+7tabish@users.noreply.github.com>
2020-07-22 11:29:38 +0500

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = 3000;
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h3>Node testing app 2</h3>');
+  res.send('<h3>Node testing app 1</h3>');
 });
 
 app.listen(PORT,()=>console.log(`App is runnning on localhost:${PORT}`));
